dd229e1c78f9ac662018f75c6c64cb80d0f102c1
Jie Chen <jiechenmc@gmail.com>
2025-08-31 19:38:36 -0400

    r0 template

--- recitation0/r0.c
+++ recitation0/r0.c
@@ -3,3 +3,7 @@
 int add(int x, int y){
     return x + y;
 }
+
+int subtract(int a, int b);
+int multiply(int a, int b);
+int divide(int a, int b);
--- recitation0/test_r0.c
+++ recitation0/test_r0.c
@@ -2,6 +2,7 @@
 #include <criterion/criterion.h>
 #include "r0.h"
 
+// This test is part of the math suite to test addition
 Test(math, addition) {
     cr_assert_eq(add(2, 3), 5, "2 + 3 should equal 5");
 }
