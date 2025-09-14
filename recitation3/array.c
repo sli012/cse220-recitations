319de5ab142dde0671164fe465584d4620b4ca23
Jie Chen <jiechenmc@gmail.com>
2025-09-14 12:39:53 -0400

    add pointer arthmetic example

--- recitation3/array.c
+++ recitation3/array.c
@@ -13,6 +13,12 @@ int main(int argc, char const *argv[])
     for (int i = 0; i < 5; i++)
         printf("i: %d addr: %p\n", i, &arr[i]); // Why are the addresses 4 bytes apart?
 
+
+    // Pointer Arithmetic 
+    // Because you know that arrays contains elements of the same size
+    // the element at index 1 is just the starting address of the array + sizeof(arr_datatype) * index
+    printf("The address at index 1 is %p, %p\n", &arr[1], (arr + 1));
+
     printf("The number of elements in the array is: %ld\n", sizeof(arr) / sizeof(arr[0]));
 
     printf("The number of elements in the array using array_len is: %d\n", array_len(arr));
