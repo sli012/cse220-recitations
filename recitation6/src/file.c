e526f04f710dbdab288da1836ae4183b67e6d28a
Jie Chen <jiechenmc@gmail.com>
2025-10-07 18:58:42 -0400

    comment out main so Criterion can run

--- recitation6/src/file.c
+++ recitation6/src/file.c
@@ -16,13 +16,13 @@ void Censor(const char *inputFile, const char *outputFile, const char *word)
 {
 }
 
-int main(int argc, char *argv[])
-{
-    if (argc < 3)
-        return EXIT_FAILURE;
+// int main(int argc, char *argv[])
+// {
+//     if (argc < 3)
+//         return EXIT_FAILURE;
 
-    // CopyFile(argv[1], argv[2]);
-    Censor(argv[1], argv[2], argv[3]);
+//     // CopyFile(argv[1], argv[2]);
+//     Censor(argv[1], argv[2], argv[3]);
 
-    return EXIT_SUCCESS;
-}
+//     return EXIT_SUCCESS;
+// }
