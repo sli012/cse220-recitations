dd4a832218debd2dd2ad45da7f33cc12fde6311d
sli012 <sl5406021@gmail.com>
2025-10-08 20:56:55 +0000

    recitation6

--- recitation6/src/file.c
+++ recitation6/src/file.c
@@ -7,22 +7,59 @@
 #define MAX_LINE 4096
 
 // TODO: implement
-void CopyFile(const char *inputFile, const char *outputFile)
-{
+void CopyFile(const char *inputFile, const char *outputFile) //takes in 2 character pointers that are names of the files
+{ 
+    FILE *in = fopen(inputFile, "r");//initialize file pointer, "r" for read
+    FILE *out = fopen(outputFile, "w"); //"w" for write
+    
+    char buffer[MAX_LINE];
+
+    while(fgets(buffer, sizeof(buffer), in)){ //fgets reads line from input file and saves it to buffer
+        fputs(buffer, out); //saves copy to output file
+    }
+
+    fclose(in);//close pointers
+    fclose(out);
 }
 
-// TODO: implement
+// string = "Hello my name is sam"
+// censor = "name"
+
+// out = "Hello my XXXX is sam"
 void Censor(const char *inputFile, const char *outputFile, const char *word)
 {
+    FILE *in = fopen(inputFile, "r");
+    FILE *out = fopen(outputFile, "w");
+
+    char buffer[MAX_LINE];
+
+    char *match; //character pointer for where a match occurs
+
+    while(fgets(buffer, sizeof(buffer), in)){ //while there are still more lines to read
+        char temp[MAX_LINE] = {0};
+        char *current = buffer;
+        
+
+        while((match = strstr(current, word))){ //loops through current which is buffer and returns instance where word appears
+            strncat(temp, current, match - current);//copy every before and after the censored word in string variable
+            strcat(temp, "XXXX");
+            current = match + strlen(word);
+        }
+        strcat(temp, current); //copies rest of string after no more matches which is what comes after the censored word in string variable
+        fputs(temp, out);
+    }
+    fclose(in);
+    fclose(out);
+
 }
 
-// int main(int argc, char *argv[])
-// {
-//     if (argc < 3)
-//         return EXIT_FAILURE;
+int main(int argc, char *argv[])
+{
+    if (argc < 3)
+        return EXIT_FAILURE;
 
-//     // CopyFile(argv[1], argv[2]);
-//     Censor(argv[1], argv[2], argv[3]);
+    // CopyFile(argv[1], argv[2]);
+    Censor(argv[1], argv[2], argv[3]);
 
-//     return EXIT_SUCCESS;
-// }
+    return EXIT_SUCCESS;
+}
--- recitation6/src/matrix.c
+++ recitation6/src/matrix.c
@@ -1,11 +1,35 @@
 #include <stdio.h>
 
 // TODO: implement
+/**
+A = 1, 2
+    3, 4
+
+A ^ T = 1, 3
+        2, 4
+
+        scr[i][j] = dest[j][i]
+
+*/
 void TransposeMatrix(int rows, int cols, int src[rows][cols], int dest[cols][rows])
 {
+    for(int i = 0; i < rows; i++){
+        for(int j = 0; j < cols; j++){
+            //dest[j][i] = src[i][j]
+            *(*(dest + j) + i) = *(*(src + i) + j); //pointer arithmetic
+            //dest[j] == *(dest + i)
+        }
+    }
 }
 
 // TODO: implement
 void RowSumOfMatrix(int rows, int cols, int src[rows][cols], int dest[rows])
 {
+    for(int i= 0; i < rows; i++){
+        *(dest + i) = 0; //initialize dest outside and set it to 0
+        for(int j = 0; i < cols; j++){
+            *(dest + i) += *(*(src+ i) +j);//add values to dest
+
+        }
+    }
 }
