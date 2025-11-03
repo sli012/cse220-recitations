4f7d6b7d5734651e589c36dff4ac4df200fd9df9
Jie Chen <jiechenmc@gmail.com>
2025-11-02 22:25:29 -0500

    up

---
+++ recitation9/calculate_age.asm
@@ -0,0 +1,7 @@
+# Hello world program
+.data
+prompt1: .asciiz "Enter your age: "
+output1: .asciiz "Your age in 5 years: "
+
+.text
+main:
---
+++ recitation9/sum.asm
@@ -0,0 +1,8 @@
+.data
+string1: .asciiz "Enter a number:"
+string2: .asciiz "Enter the second number:"
+string3: .asciiz "Sum:"
+endLine: .asciiz "\n"
+
+.text
+main:
