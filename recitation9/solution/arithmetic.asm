0c01ad4f43bd246cf212d7f0862c11ca457a276b
Jie Chen <jiechenmc@gmail.com>
2025-11-02 22:23:44 -0500

    save

--- recitation9/solution/arithmetic.asm
+++ recitation9/solution/arithmetic.asm
@@ -3,7 +3,7 @@ string1: .asciiz "Enter a number : "
 endLine: .asciiz "\n"
 string2: .asciiz "Addition result is  : "
 string3: .asciiz "Substraction result is : "
-string4: .asciiz "Your second number is greater than your first number and then you can not sub these two numbers : "
+string4: .asciiz "Result is : "
 
 .text
 main:
@@ -27,14 +27,12 @@ li $v0,5
 syscall
 move $t1,$v0
 
-
-
-
-beq $t0,$t1, Addition
-j Substraction
+# Check conditions
+beq $t0,$t1, Addition      # If numbers are equal, do addition
+bgt $t0,$t1, Substraction  # If first > second, do subtraction
+j ReturnZero               # Otherwise, return 0
 
 Addition:
-
 add $t2,$t1,$t0
 li $v0,4
 la $a0,string2    #cout<<Addition result is 
@@ -45,15 +43,10 @@ move $a0,$t2
 syscall
 j end_label
 
-
 Substraction:
-bgt $t1,$t0, EnteraNumber2
-j SUB2
-
-SUB2:
 sub $t2,$t0,$t1
 li $v0,4
-la $a0,string3    #cout<<Addition result is 
+la $a0,string3    #cout<<Substraction result is 
 syscall
 
 li $v0,1
@@ -61,38 +54,17 @@ move $a0,$t2
 syscall
 j end_label
 
-
-
-EnteraNumber2:
-
+ReturnZero:
+li $t2, 0
 li $v0,4
-la $a0,string4
+la $a0,string4    #cout<<Result is 
 syscall
 
-li $v0,4
-la $a0,endLine
+li $v0,1
+move $a0,$t2
 syscall
-
-li $v0,4
-la $a0,string1
-syscall
-
-li $v0,5
-syscall
-move $t0,$v0
-
-li $v0,4
-la $a0,string1    #cout<<Enter a number
-syscall
-
-li $v0,5
-syscall
-move $t1,$v0
-j Substraction
-
+j end_label
 
 end_label:
 li $v0, 10        #exit
 syscall
-
-
